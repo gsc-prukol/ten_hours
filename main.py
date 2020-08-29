7e1907cbb2634124e64111f11d7fa1c2535fbb71
volodymyr_volodko <volodymyr.volodko@evinent.com>
2020-08-29 23:23:57 +0300

    refactoring

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import numpy as np
 import time
 
 video_path = "E:/test.mp4"
-p_frame_thresh = 150  # You may need to adjust this threshold
-count_unique_frame = 720
+p_frame_thresh = 110  # You may need to adjust this threshold
+count_unique_frame = 725
+start_frame_number = 296000
 window_name = 'Image'
 color_red = (255,0,0)
 update_fps = 60
 cap = cv2.VideoCapture(video_path)
+cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame_number)
 example_frame = dict()
 index_collision = 0
 count_frame = 0
@@ -16,9 +18,13 @@ unique_frame_count = 0
 ret = True
 time_start = time.time()
 fps = 1
-def get_list(dict, index, limit = 15):
+def get_list(dict, index, limit = 20, last = 2):
     tmp = list(dict.keys())
-    tmp2 = tmp[index - 1:] + tmp[:index - 1]
+    index = index - last
+    if index < 0:
+        index = len(tmp) + index
+
+    tmp2 = tmp[index:] + tmp[:index]
 
     return tmp2[:limit]
 
@@ -26,8 +32,9 @@ while ret:
     count_frame += 1
     if count_frame % update_fps == 0:
         current_time = time.time()
-        fps = update_fps / (current_time - time_start)
+        fps = int(update_fps / (current_time - time_start))
         time_start = current_time
+
     ret, curr_frame = cap.read()
     curr_frame = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)
     if ret:
@@ -44,17 +51,15 @@ while ret:
                     break
             if unique:
                 unique_frame_count += 1
-                name_image = f"unique_frames/unique_image_{count_frame}.png"
+                name_image = f"unique_frames/unique_image_{count_frame + start_frame_number}.png"
                 cv2.imwrite(name_image, curr_frame)
                 print(f"Add frame number {unique_frame_count} to unique set.")
             else:
-                print(f"FPS: {fps}, has {unique_frame_count} unique frame, index collision = {index_collision}")
-                cv2.putText(curr_frame, f"FPS: {fps}, frame {count_frame} is exist", (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, color_red, 2)
-
+                cv2.putText(curr_frame, f"FPS: {fps}, frame {count_frame + start_frame_number} is exist", (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, color_red, 2)
 
         cv2.imshow(window_name, curr_frame)
         key = cv2.waitKey(1) & 0xFF
 
         # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
+        if key == ord("q") or key == ord("й"):
             break
